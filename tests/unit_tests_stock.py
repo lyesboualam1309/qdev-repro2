11e79fdc9b40a1d89a7bd65195be9bc65605c0c0
Marek Felšöci <marek@felsoci.sk>
2026-04-16 23:50:49 +0200

    Préparer l'implémentation du test unitaire de « verifierCommandeImpression »

---
+++ tests/unit_tests_stock.py
@@ -0,0 +1,9 @@
+import unittest
+from repro2.stock import *
+
+class TestVerifierCommandeImpression(unittest.TestCase):
+  def test_exemple(self):
+    self.assertEqual(True, False)
+
+if __name__ == '__main__':
+  unittest.main()
