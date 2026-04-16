75b4ffe0ff8d8bedaaf4b681b06df96fac224c63
Marek Felšöci <marek@felsoci.sk>
2026-04-16 23:50:33 +0200

    Préparer l'implémentation du test unitaire de « codeCommandeConforme »

---
+++ tests/unit_tests_commande.py
@@ -0,0 +1,9 @@
+import unittest
+from repro2.commande import *
+
+class TestCodeCommandeConforme(unittest.TestCase):
+  def test_exemple(self):
+    self.assertEqual(True, False)
+
+if __name__ == '__main__':
+  unittest.main()
