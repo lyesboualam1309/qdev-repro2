b1863955229c7c1d1993c7a6572230e65ab130cd
lyesboualam1309 <lyesboualam1309@gmail.com>
2026-04-17 11:35:39 +0200

    Revise test_exemple for verifierCommandeImpression
    Updated the test case for verifierCommandeImpression to include multiple assertions and error handling.

--- tests/unit_tests_feuilles.py
+++ tests/unit_tests_feuilles.py
@@ -2,8 +2,14 @@ import unittest
 from repro2.feuilles import *
 
 class TestVerifierCommandeImpression(unittest.TestCase):
-  def test_exemple(self):
-    self.assertEqual(True, False)
+    def test_exemple(self):
+        self.assertEqual(verifierCommandeImpression(100, 300), Reponse.ACCEPTER_SANS_ALERTE)
+        self.assertEqual(verifierCommandeImpression(100, 140), Reponse.ACCEPTER_ALERTE_ORANGE)
+        self.assertEqual(verifierCommandeImpression(100, 100), Reponse.ACCEPTER_ALERTE_ROUGE)
+        self.assertEqual(verifierCommandeImpression(100, 90), Reponse.REFUSER)
+        self.assertEqual(verifierCommandeImpression(2001, 3000), Reponse.REFUSER)
+        with self.assertRaises(ValueError):
+            verifierCommandeImpression(0, 100)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
