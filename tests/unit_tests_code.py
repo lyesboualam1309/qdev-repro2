6d74fa5f50f418f6984a1465367b1c3bffe167b8
lyesboualam1309 <lyesboualam1309@gmail.com>
2026-04-17 11:34:47 +0200

    Enhance unit tests for codeCommandeConforme function
    Updated the test case for codeCommandeConforme to include various scenarios and error handling.

--- tests/unit_tests_code.py
+++ tests/unit_tests_code.py
@@ -2,8 +2,15 @@ import unittest
 from repro2.code import *
 
 class TestCodeCommandeConforme(unittest.TestCase):
-  def test_exemple(self):
-    self.assertEqual(True, False)
+    def test_exemple(self):
+        self.assertEqual(codeCommandeConforme("#22ABg12"),True)
+        self.assertEqual(codeCommandeConforme("$22ABg12"),False)
+        self.assertEqual(codeCommandeConforme("#2AAbg12"),False)
+        self.assertEqual(codeCommandeConforme("#22ABg1+"),False)
+        with self.assertRaises(ErreurTropCourt):
+            codeCommandeConforme("#22")
+        with self.assertRaises(ErreurTropLong):
+            codeCommandeConforme("#22ABg12963")
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
