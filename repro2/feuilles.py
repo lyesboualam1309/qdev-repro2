0d94d1bbfc7d907079aa82244f104ea682437d9d
lyesboualam1309 <lyesboualam1309@gmail.com>
2026-04-17 11:28:32 +0200

    Refactor Reponse enum and implement verifierCommandeImpression
    Refactored Reponse enum and implemented verifierCommandeImpression function to check print order conditions.

--- repro2/feuilles.py
+++ repro2/feuilles.py
@@ -1,10 +1,22 @@
 from enum import Enum
 
 class Reponse(Enum):
-  ACCEPTER_SANS_ALERTE = 0,
-  ACCEPTER_ALERTE_ORANGE = 1,
-  ACCEPTER_ALERTE_ROUGE = 2,
-  REFUSER = 3
+    ACCEPTER_SANS_ALERTE = 0
+    ACCEPTER_ALERTE_ORANGE = 1
+    ACCEPTER_ALERTE_ROUGE = 2
+    REFUSER = 3
 
-def verifierCommandeImpression(nbFeuillesNecessaires : int, nbFeuillesEnStock : int) -> Reponse:
-  pass
+def verifierCommandeImpression(nbFeuillesNecessaires: int, nbFeuillesEnStock: int) -> Reponse:
+    if nbFeuillesNecessaires <= 0:
+        raise ValueError("Erreur")
+    if nbFeuillesNecessaires > 2000:
+        return Reponse.REFUSER
+    difference = nbFeuillesEnStock - nbFeuillesNecessaires
+    if difference > 50:
+        return Reponse.ACCEPTER_SANS_ALERTE
+    if 1 <= difference <= 50:
+        return Reponse.ACCEPTER_ALERTE_ORANGE
+    if difference == 0:
+        return Reponse.ACCEPTER_ALERTE_ROUGE
+    if difference < 0:
+        return Reponse.REFUSER
